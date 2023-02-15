86368970567c671b0aa5e7e3cb4ee60af4d0b10a
BS-GavinC <113683718+BS-GavinC@users.noreply.github.com>
2023-02-15 12:43:22 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BindingsComponent } from './modules/demo/bindings/bindings.component';
+import { MainDemoComponent } from './modules/demo/main-demo/main-demo.component';
+import { PipesComponent } from './modules/demo/pipes/pipes.component';
+import { ChronometreComponent } from './modules/exercice/chronometre/chronometre.component';
+import { MainExerciceComponent } from './modules/exercice/main-exercice/main-exercice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'demo', component : MainDemoComponent, children : [
+    {path : 'bindings', component : BindingsComponent},
+    {path : 'pipes', component : PipesComponent}
+  ]},
+  {path : 'exercice', component : MainExerciceComponent, children : [
+    {path : 'chronometre', component : ChronometreComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
+import { MainDemoComponent } from './modules/demo/main-demo/main-demo.component';
+import { MainExerciceComponent } from './modules/exercice/main-exercice/main-exercice.component';
+import { BindingsComponent } from './modules/demo/bindings/bindings.component';
+import { FormsModule } from '@angular/forms';
+import { PipesComponent } from './modules/demo/pipes/pipes.component';
+import { TempPipe } from './shared/pipes/temp.pipe';
+import { ChronometreComponent } from './modules/exercice/chronometre/chronometre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    MainDemoComponent,
+    MainExerciceComponent,
+    BindingsComponent,
+    PipesComponent,
+    TempPipe,
+    ChronometreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
