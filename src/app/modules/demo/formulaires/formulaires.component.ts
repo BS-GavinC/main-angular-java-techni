ae1b523350b8e5a1035e1ad872675d123587aa0a
BS-GavinC <113683718+BS-GavinC@users.noreply.github.com>
2023-02-22 08:40:10 +0100

    forms

--- src/app/modules/demo/formulaires/formulaires.component.ts
+++ src/app/modules/demo/formulaires/formulaires.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaires',
@@ -8,7 +8,65 @@ import { AbstractControl, FormBuilder, FormControl, ValidationErrors, ValidatorF
 })
 export class FormulairesComponent {
 
-  constructor(){}
+  constructor(private _formBuilder : FormBuilder){}
+
+
+  myFormGroup : FormGroup = this._formBuilder.group({
+    email : ['', [Validators.email, Validators.required]],
+    username : ['', [Validators.required, this.moreThanFour()]],
+    password : ['', [Validators.required, Validators.minLength(8)]]
+  })
+
+  submit(){
+    let values = this.myFormGroup.value;
+
+    console.log(values.username);
+
+    console.log(this.myFormGroup.value['password']);
+  }
+
+  // FORM ARRAY
+
+  myFormGroupWithArray : FormGroup = this._formBuilder.group({
+
+    myArray : this._formBuilder.array([
+      new FormControl('', Validators.required)
+    ])
+  })
+
+  getArray() : FormArray {
+    return this.myFormGroupWithArray.get('myArray') as FormArray
+  }
+
+  addControl(){
+    this.getArray().push(new FormControl('', Validators.required))
+  }
+
+  removeControl(index : number){
+    this.getArray().removeAt(index)
+  }
+
+  submitArray(){
+    let values = this.myFormGroupWithArray.value;
+
+    console.log(values['myArray']);
+
+    this.getArray().clear()
+
+  }
+
+  // Custom Validator
+
+  moreThanFour() : ValidatorFn {
+
+    return (control : AbstractControl) : ValidationErrors | null => {
+
+      let value = control.value;
+      return value.length > 4 ? null : {tabuse : true}
+
+    }
+
+  }
 
 
 
